87376e8c8bc8e8ffaa580af50a652ab119d06b76
Tony Watkins <tony@llifion.net>
2025-01-21 13:35:34 +0000

    feature: add iterator for mutable vec slice

--- src/lib.rs
+++ src/lib.rs
@@ -12,12 +12,36 @@ impl<'a, T> Iterator for MyIterator<'a, T> {
     }
 }
 
+struct MyMutableIterator<'iter, T> {
+    slice: &'iter mut [T],
+}
+
+impl<'iter, T> Iterator for MyMutableIterator<'iter, T> {
+    type Item = &'iter mut T;
+
+    fn next<'next>(&'next mut self) -> Option<Self::Item> {
+        let slice = &mut self.slice;
+
+        // Using shadowing, temp put an empty slice into self slice
+        // and have our temp slice (pointer to pointer) point to the original memory.
+        // Temp slice can now be manipulated, splitting off first and rest
+        // after which they can be reassigned to our struct slice element
+        // Temp slice variable lasts as long as 'iter
+
+        let slice = std::mem::replace(slice, &mut []);
+        let (first, rest) = slice.split_first_mut()?;
+        self.slice = rest;
+        Some(first)
+
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn test_immutable_vec() {
         let collection = vec![1, 2, 3, 4];
         let wrapper = MyIterator {
             slice: &collection[..],
@@ -25,5 +49,19 @@ mod tests {
         for (index, elem) in wrapper.enumerate() {
             assert_eq!(*elem, collection[index]);
         }
+
+    }
+
+    #[test]
+    fn test_mutable_vec() {
+        let mut collection = vec![1, 2, 3, 4];
+        let wrapper = MyMutableIterator {
+            slice: &mut collection[..],
+        };
+        for (_index, elem) in wrapper.enumerate() {
+            *elem = *elem + 1;
+
+        }
+        assert_eq!(collection.get(0), Some(&2) );
     }
 }
